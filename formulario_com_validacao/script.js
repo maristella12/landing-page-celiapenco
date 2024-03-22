7a74e3f8889881055300eaec1b3caf90619182d8
Maristella Bittencourt <tellabitt12@gmail.com>
2024-03-22 14:07:28 -0300

    alteração de formulário_2

--- formulario_com_validacao/script.js
+++ formulario_com_validacao/script.js
@@ -7,23 +7,83 @@ const passwordConfirmation = document.getElementById('passwordConfirmation')
 
 form.addEventListener('submit',(event)=>{
 event.preventDefault();
+
+checkInputUsername();
+checkInputEmail();
+checkInputPassword();
+checkInputPasswordConfirmation();
 })
 
+//PEGA O TEXTO INSERIDO NO CAMPO
 function checkInputUsername(){
-const usernameValue = username.value 
 
-if(username === ""){
+const usernameValue = username.value;
+
+if(usernameValue === ""){
+//MOSTRAR O AVISO E MOSTRAR A MENSAGEM DE ERRO... 
+//ATRAVÉS DA CHAMADA DA FUNÇÃO
 errorInput(username, "Preencha um username!")
-}
+   }else{
+const formItem = username.parentElement;
+                 formItem.className = 'form-content'
+   }
 
 }
+
+function checkInputEmail(){
+const emailValue = email.value;
+if(emailValue === ""){
+errorInput(email,"O e-mail é obrigatório.")
+}else{
+   const formItem = email.parentElement;
+    formItem.className = 'form-content'
+   }
+
+}
+
+
+
+function checkInputPassword(){
+const passwordValue = password.value;
+
+if(passwordValue === ""){
+   errorInput(password,"A senha é obrigatória.")
+}else if(passwordValue.length<8){
+   errorInput(password,"A senha precisa ter no mínimo 8 caracteres")
+}else{
+const formItem = password.parentElement;
+                 formItem.className = 'form-content'
+}
+   }
+
+
+
+function checkInputPasswordConfirmation(){
+const passwordValue = password.value;
+const confirmationPasswordValue = passwordConfirmation.value;
+
+if(confirmationPasswordValue === ""){
+   errorInput(passwordConfirmation,"A confirmação de senha é obrigatória.")
+}else if(confirmationPasswordValue !==passwordValue){
+   errorInput(passwordConfirmation,"As senhas não são iguais")
+}else{
+const formItem = passwordConfirmation.parentElement;
+                 formItem.className = 'form-content'
+}
+   }
+
+
+
+
+
 
 function errorInput(input,message){
-const formItem = input.parentElement;
-const textMessage = formItem.querySelector('a')
-
+const formItem = input.parentElement; //ACESSA O ELEMENTO PAI QUE É A DIV 'FORM-CONTENT'
+const textMessage = formItem.querySelector('a') // ACESSA O ITEM 'a' DESSA DIV
+//atualiza o valor do texto para a 'message'
 textMessage.innerText = message;
 
 formItem.className = 'form-content error'
 
 }
+
